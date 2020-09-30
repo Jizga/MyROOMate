f47b5a526c028237b445904d33150a68d48e688c
Jizga <jizga93@gmail.com>
2020-10-01 00:12:21 +0200

    start with Profile's page

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,5 +1,8 @@
 import React from "react";
-import User from "../assets/images/whiteUser.png";
+import User from "../assets/images/user.png";
+import email from "../assets/images/email.png";
+import mobile from "../assets/images/telefono-inteligente.png";
+import country from "../assets/images/country.png";
 import "./Profile.css";
 
 export default function Profile() {
@@ -9,7 +12,34 @@ export default function Profile() {
         <h3 className="T">Your Personal Area</h3>
       </div>
       <div className="body">
-        <img className="user" src={User} alt="user"></img>
+        <img className="user" src={User} alt="user" />
+        <div className="profile">
+          <div className="titleProfile">Profile</div>
+          <div className="fullName">
+            Full Name
+            <input className="fullN" />
+          </div>
+          <div className="email">
+            <img className="email" src={email} alt="email" />
+            <input className="email" />
+          </div>
+          <div className="mobile">
+            <img className="mobile" src={mobile} alt="mobile" />
+            <input className="mobile" />
+          </div>
+          <div className="country">
+            <img className="country" src={country} alt="country" />
+            <input className="country" />
+          </div>
+          <div className="years">
+            Years Old
+            <input className="years" />
+          </div>
+          <div className="history">
+            Tell us a little about you
+            <input className="history" />
+          </div>
+        </div>
       </div>
     </div>
   );
